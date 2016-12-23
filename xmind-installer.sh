cee93c99e36c822989dcf1156f2bed8fae2e67c4
mriza <muhammadriza@gmail.com>
2016-12-24 02:15:40 +0700

    fixing errors
    fixing fatal errors

--- xmind-installer.sh
+++ xmind-installer.sh
@@ -17,27 +17,28 @@
 
 USER=$1
 ARCH=`uname -m`
-if $ARCH='x86_64' then
-	DIR='XMind_amd64'
-elif $ARCH='i686' then
-	DIR='XMind_i386'
+if [ $ARCH = "x86_64" ]
+then
+	XMIN_DIR="/opt/xmind/XMind_amd64"
+elif [ $ARCH = "i686" ]
+then
+	XMIND_DIR="/opt/xmind/XMind_i386"
 else
 	echo 'Sorry, cannot verify your kernel version'
 	echo 'The installer will now exit'
 	exit
 fi
 
-unzip xmind-8-linux.zip -d xmind
+unzip -q xmind-8-linux.zip -d xmind
 mv xmind /opt/
-cd /opt/xmind
 mkdir -p /usr/share/fonts/truetype/xmind
-cp -R fonts/* /usr/share/fonts/truetype/xmind/
+cp -R /opt/xmind/fonts/* /usr/share/fonts/truetype/xmind/
 fc-cache -f
 
 cat <<EOF | tee /usr/share/applications/xmind8.desktop
 [Desktop Entry]
 Comment=Create and share mind maps.
-Exec=/opt/xmind/$(DIR)/XMind %F
+Exec=/opt/xmind/$DIR/XMind %F
 Name=XMind
 Terminal=false
 Type=Application
@@ -45,12 +46,9 @@ Categories=Office;
 Icon=xmind
 EOF
 
-sed -i "s/\.\.\/workspace/@user\.home\/workspace/" /opt/xmind/$(DIR)/XMind.ini
-sed -i "s/\.\/configuration/@user\.home\/\.configuration/" /opt/xmind/$(DIR)/XMind.ini
-sed -i "s/^\.\./\/opt\/xmind/" /opt/xmind/$(DIR)/XMind.ini
+sed -i "s/\.\.\/workspace/@user\.home\/workspace/" $XMIND_DIR/XMind.ini
+sed -i "s/\.\/configuration/$XMIND_DIR/\.configuration/" $XMIND_DIR/XMind.ini
+sed -i "s/^\.\./\/opt\/xmind/" $XMIND_DIR/XMind.ini
 
-
-mkdir /home/$(USER)/workspace
-cp -R /opt/xmind/$(DIR)/configuration /home/$(USER)/
-chown -R $(user): /home/$(USER)/.configuration
-chown -R $(user): /home/$(USER)/workspace
+mkdir /home/$USER/workspace
+chown -R $USER: /home/$USER/workspace
