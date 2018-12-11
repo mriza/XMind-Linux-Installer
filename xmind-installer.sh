c920b1a2ee1eec09ed4cf4e7482d820132925065
danglduy <38782010+danglduy@users.noreply.github.com>
2018-12-11 19:17:21 +0700

    hide workspace folder

--- xmind-installer.sh
+++ xmind-installer.sh
@@ -68,10 +68,10 @@ EOF
 
 cnfg(){
 	echo "Creating workspace and configuration..."
-	mkdir /home/$1/{workspace,.configuration}
+	mkdir /home/$1/{.workspace,.configuration}
 	cp -R $BIN_DIR/configuration/* /home/$1/.configuration
-	chown -R $1: /home/$1/{workspace,.configuration}
-	sed -i "s/\.\.\/workspace/@user\.home\/workspace/g" "$BIN_DIR/XMind.ini"
+	chown -R $1: /home/$1/{.workspace,.configuration}
+	sed -i "s/\.\.\/workspace/@user\.home\/.workspace/g" "$BIN_DIR/XMind.ini"
 	sed -i "s/\.\/configuration/@user\.home\/\.configuration/g" "$BIN_DIR/XMind.ini"
 	sed -i "s/^\.\./\/opt\/xmind/g" "$BIN_DIR/XMind.ini"
 }
